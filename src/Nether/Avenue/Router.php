0a2c38fc689dad35f10d6d44d64b4e7301108d69
bobmagicii <bob@majdak.net>
2020-05-27 20:11:47 +0000

    make GetRemoteAddr nullable.

--- src/Nether/Avenue/Router.php
+++ src/Nether/Avenue/Router.php
@@ -295,7 +295,7 @@ class Router {
 
 	public function
 	GetRemoteAddr():
-	String {
+	?String {
 	/*//
 	@date 2020-05-27
 	fetch the request ip address checking that it even exists in the case
@@ -305,7 +305,7 @@ class Router {
 		if(array_key_exists('REMOTE_ADDR',$_SERVER))
 		return $_SERVER['REMOTE_ADDR'];
 
-		return '';
+		return NULL;
 	}
 
 	////////////////
