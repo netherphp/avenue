ceb5a07dc8fd545608f45a14b54be801700f480e
bobmagicii <bob@majdak.net>
2024-06-24 06:08:19 +0000

    thusfar

--- tests/Nether/Avenue/RequestTest.php
+++ tests/Nether/Avenue/RequestTest.php
@@ -38,7 +38,6 @@ extends PHPUnit\Framework\TestCase {
 		$_SERVER['HTTP_HOST'] = 'avenue-magic.test';
 		$_SERVER['REQUEST_URI'] = '/test/magic?arg=one&with=query';
 		$_POST = [ 'item'=> 'thing' ];
-		$_GET = [ 'arg'=> 'one' ];
 
 		$Req = new Request;
 		$Req->ParseRequest();
